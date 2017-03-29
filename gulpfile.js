04ae4de5c4ce2e3dd5a2ceb9ff6b01627b5b255a
Suz Campbell <suz.blandon@gmail.com>
2017-03-28 20:03:27 -0600

    Added navigation skeleton

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp =require ('gulp');
+var watch = require ('gulp-watch');
+var batch = require ('gulp-batch');
+var postcss = require ('gulp-postcss');
+var sourcemaps = require ('gulp-sourcemaps');
+var autoprefixer = require ('autoprefixer');
+
+gulp.task('autoprefixer', function () {
+    return gulp.src('*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./compiled'));
+});
+
+gulp.task('watch', function () {
+    watch('*.css', batch(function (events, done) {
+        gulp.start('autoprefixer', done);
+    }));
+});
+
+gulp.task('default', ['watch']);
